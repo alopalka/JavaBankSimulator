849c0d99a03375ab6c36fcf1794b4f1e36bd0bf0
s24279 <jakubwiecek93@gmail.com>
2022-11-06 16:37:37 +0100

    litlle fix

--- src/main/java/com/bank/spring/jpa/client/ClientRepository.java
+++ src/main/java/com/bank/spring/jpa/client/ClientRepository.java
@@ -1,9 +1,7 @@
 package com.bank.spring.jpa.client;
 
 import com.bank.spring.jpa.client.model.Client;
-import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
