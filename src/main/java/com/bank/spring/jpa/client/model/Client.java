6f7581d87df8ea743cec78a89f01247b6055c20b
s24279 <jakubwiecek93@gmail.com>
2022-11-11 13:22:59 +0100

    client id

--- src/main/java/com/bank/spring/jpa/client/model/Client.java
+++ src/main/java/com/bank/spring/jpa/client/model/Client.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Client {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)git st
     private long id;
 
     @Column(name = "firstName")
