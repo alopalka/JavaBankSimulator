a27f70524cec937227ee227df7248d4f9c5f6e3f
alopalka <d3bian@mopalka.idl.pl>
2022-11-11 12:54:33 +0100

    Added password config. Added default user

--- src/main/java/com/bank/spring/jpa/security/SecurityConfiguration.java
+++ src/main/java/com/bank/spring/jpa/security/SecurityConfiguration.java
@@ -1,18 +1,45 @@
 package com.bank.spring.jpa.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception{
+    protected UserDetailsService userDetailsManager() {
+        UserDetails user1 = User.builder()
+                .username("test")
+                .password(passwordEncoder.encode("test1"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1);
+    }
+
+    // DELETE THIS IN THE FUTURE TERRIBLE SOLUTION FOR PRODUCTION
+
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
                 .authorizeRequests()
                 .antMatchers("/")
